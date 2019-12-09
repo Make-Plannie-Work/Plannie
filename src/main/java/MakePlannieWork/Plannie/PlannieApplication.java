9567aed6ea1ce09878664742efd07f35fca1b74d
Daniel Kuperus <daniel.kuperus@gmail.com>
2019-12-09 15:47:34 +0100

    Project aangemaakt.

---
+++ src/main/java/MakePlannieWork/Plannie/PlannieApplication.java
@@ -0,0 +1,13 @@
+package MakePlannieWork.Plannie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MakePlannieWork/Plannie/PlannieApplicationTests.java
@@ -0,0 +1,13 @@
+package MakePlannieWork.Plannie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
