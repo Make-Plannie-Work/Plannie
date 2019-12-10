e3e9eb11808ddb5ebcce07b35f5ca6f80bd42a22
Wouter Meindertsma <wesmeindertsma@gmail.com>
2019-12-10 13:32:52 +0100

    3.1 Object gebruiker is aangemaakt.

--- src/main/java/model/Gebruiker.java
+++ src/main/java/model/Gebruiker.java
@@ -1,4 +1,53 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
 public class Gebruiker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer gebruikersId;
+    private String voornaam;
+    private String achternaam;
+    private String wachtwoord;
+
+    @Transient
+    private String trancientWachtwoord;
+
+    public String toString() {
+        return gebruikersId + ":\t" + voornaam + " " + achternaam;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public String getWachtwoord() {
+        return wachtwoord;
+    }
+
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
+    }
+
+    public String getTrancientWachtwoord() {
+        return trancientWachtwoord;
+    }
+
+    public void setTrancientWachtwoord(String trancientWachtwoord) {
+        this.trancientWachtwoord = trancientWachtwoord;
+    }
 }
