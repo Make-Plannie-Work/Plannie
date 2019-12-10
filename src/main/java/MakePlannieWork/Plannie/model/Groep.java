25e23323f2c016af8ac4a1a773d2548514c0422e
Wouter Meindertsma <wesmeindertsma@gmail.com>
2019-12-10 16:04:13 +0100

    6.1
    - Model Groep is aangemaakt
    - ManytoMany relatie met Gebruiker

--- src/main/java/MakePlannieWork/Plannie/model/Groep.java
+++ src/main/java/MakePlannieWork/Plannie/model/Groep.java
@@ -1,9 +1,7 @@
 package MakePlannieWork.Plannie.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Groep {
@@ -11,4 +9,33 @@ public class Groep {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer groepId;
+
+    private String groepsNaam;
+
+    @ManyToMany
+    private Set<Gebruiker> groepsleden;
+
+    public Integer getGroepId() {
+        return groepId;
+    }
+
+    public void setGroepId(Integer groepId) {
+        this.groepId = groepId;
+    }
+
+    public String getGroepsNaam() {
+        return groepsNaam;
+    }
+
+    public void setGroepsNaam(String groepsNaam) {
+        this.groepsNaam = groepsNaam;
+    }
+
+    public Set<Gebruiker> getGroepsleden() {
+        return groepsleden;
+    }
+
+    public void setGroepsleden(Set<Gebruiker> groepsleden) {
+        this.groepsleden = groepsleden;
+    }
 }
