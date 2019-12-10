554798a99b226c1d20f8aa5e0adbc76eaebb9a56
DanielKuperus <daniel.kuperus@gmail.com>
2019-12-10 13:09:36 +0100

    Test stubs toegevoegd.

---
+++ src/test/java/controller/GebruikerControllerTest.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GebruikerControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/controller/ReisItemControllerTest.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReisItemControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
