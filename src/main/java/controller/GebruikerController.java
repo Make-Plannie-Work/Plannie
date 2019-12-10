ec3d7eb8ac3cb27c7335b0217cdc15e25871c858
Wouter Meindertsma <wesmeindertsma@gmail.com>
2019-12-10 14:03:47 +0100

    3.3 - Controller is klaar voor het aanmaken van een nieuwe gebruiker.

--- src/main/java/controller/GebruikerController.java
+++ src/main/java/controller/GebruikerController.java
@@ -1,9 +1,11 @@
 package controller;
 
+import model.Gebruiker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import repository.GebruikerRepository;
 
 @Controller
@@ -12,10 +14,25 @@ public class GebruikerController {
     @Autowired
     private GebruikerRepository gebruikerRepository;
 
+    @GetMapping({"/index" , "/"})
+    String index(Model model) {
+        model.addAttribute("loginForm", new Gebruiker());
+        return "index";
+    }
+
     @GetMapping("/registreren")
     public String registreren(Model model) {
         model.addAttribute("registratieFormulier", model);
         return "registreren";
     }
 
+    @PostMapping("/registreren")
+    public String nieuweGebruiker(Gebruiker gebruiker, Model model) {
+        model.addAttribute(gebruiker);
+        if (gebruiker != null && gebruiker.getWachtwoord().equals(gebruiker.getTrancientWachtwoord())) {
+            gebruikerRepository.save(gebruiker);
+            return index(model);
+        }
+        return "registratieError";
+    }
 }
