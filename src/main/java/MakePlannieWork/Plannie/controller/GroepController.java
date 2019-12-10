3b0c38cebb35ac2ef1a7d6afc0f21eae62fd444c
Wouter Meindertsma <wesmeindertsma@gmail.com>
2019-12-10 16:26:47 +0100

    6.2
    - Groep Controller is aangemaakt.

--- src/main/java/MakePlannieWork/Plannie/controller/GroepController.java
+++ src/main/java/MakePlannieWork/Plannie/controller/GroepController.java
@@ -1,7 +1,39 @@
 package MakePlannieWork.Plannie.controller;
 
+import MakePlannieWork.Plannie.model.Groep;
+import MakePlannieWork.Plannie.repository.GroepRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GroepController {
+
+    @Autowired
+    private GroepRepository groepRepository;
+
+    @GetMapping("/groepAanmaken")
+    public String nieuweGroep(Model model) {
+        model.addAttribute("nieuweGroepFormulier", model);
+        return "groepNieuw";
+    }
+
+    @PostMapping("/groepAanmaken")
+    public String nieuweGroep (Groep groep, Model model) {
+        model.addAttribute(groep);
+        if (groep != null && !groep.getGroepsNaam().isEmpty()) {
+            groepRepository.save(groep);
+            return groepDetail(groep, model);
+        }
+        return "nieuweGroepError";
+    }
+
+    @GetMapping("/groepDetail")
+    public String groepDetail(Groep groep, Model model) {
+        model.addAttribute(groep);
+        return "groepDetail";
+    }
+
 }
