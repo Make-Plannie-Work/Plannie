fff14e69258517046303aac4607c11709ed01cef
Wouter Meindertsma <wesmeindertsma@gmail.com>
2019-12-11 15:59:42 +0100

    1.1
    - Security werkt

--- src/main/java/MakePlannieWork/Plannie/controller/GebruikerController.java
+++ src/main/java/MakePlannieWork/Plannie/controller/GebruikerController.java
@@ -2,9 +2,12 @@ package MakePlannieWork.Plannie.controller;
 
 import MakePlannieWork.Plannie.model.Gebruiker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import MakePlannieWork.Plannie.repository.GebruikerRepository;
 
@@ -12,27 +15,31 @@ import MakePlannieWork.Plannie.repository.GebruikerRepository;
 public class GebruikerController {
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private GebruikerRepository gebruikerRepository;
 
     @GetMapping({"/index" , "/"})
-    String index(Model model) {
-        //MakePlannieWork.Plannie.model.addAttribute("loginForm", new Gebruiker());
+    String index() {
+        //MakePlannieWork.Plannie.MakePlannieWork.Plannie.model.addAttribute("loginForm", new Gebruiker());
         return "index";
     }
 
     @GetMapping("/registreren")
     public String registreren(Model model) {
-        model.addAttribute("registratieFormulier", model);
+        model.addAttribute("registratieFormulier", new Gebruiker());
         return "gebruikerNieuw";
     }
 
     @PostMapping("/registreren")
-    public String nieuweGebruiker(Gebruiker gebruiker, Model model) {
-        model.addAttribute(gebruiker);
-        if (gebruiker != null && gebruiker.getWachtwoord().equals(gebruiker.getTrancientWachtwoord())) {
+    public String nieuweGebruiker(@ModelAttribute("registratieformulier") Gebruiker gebruiker, BindingResult result) {
+        if (result.hasErrors() || !gebruiker.getWachtwoord().equals(gebruiker.getTrancientWachtwoord())) {
+            return "gebruikerNieuw";
+        } else {
+            gebruiker.setWachtwoord(passwordEncoder.encode(gebruiker.getWachtwoord()));
             gebruikerRepository.save(gebruiker);
-            return index(model);
+            return "redirect:/registreren";
         }
-        return "registratieError";
     }
 }
--- src/main/java/MakePlannieWork/Plannie/repository/GebruikerRepository.java
+++ src/main/java/MakePlannieWork/Plannie/repository/GebruikerRepository.java
@@ -5,7 +5,11 @@ import MakePlannieWork.Plannie.model.Groep;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GebruikerRepository extends JpaRepository<Gebruiker, Integer> {
+
+    Optional<Gebruiker> findByEmail (String email);
+
     List<Gebruiker> findByGroepen_groepsNaam(String groepsNaam);
 }
