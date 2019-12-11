4b97ea839991495fc9c7b4be70a9f31ec72258d8
Wouter Meindertsma <wesmeindertsma@gmail.com>
2019-12-11 13:31:55 +0100

    3.7
    ApplicatieTest: Sanity test voor Controller toegevoegd.

--- src/test/java/MakePlannieWork/Plannie/PlannieApplicationTests.java
+++ src/test/java/MakePlannieWork/Plannie/PlannieApplicationTests.java
@@ -1,13 +1,24 @@
 package MakePlannieWork.Plannie;
 
+import MakePlannieWork.Plannie.controller.GebruikerController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PlannieApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private GebruikerController gebruikerController;
 
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(gebruikerController).isNotNull();
+	}
 }
