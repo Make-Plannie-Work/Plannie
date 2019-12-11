880e276f9e18d9466b37979bedbc127117318892
DanielKuperus <daniel.kuperus@gmail.com>
2019-12-11 09:31:29 +0100

    De CromeDriver null pointer bug opgelost. Het moet nog wel in een andere TestModule komen, en de setup and tear down moet gebruikt gaan worden.

--- src/test/java/MakePlannieWork/Plannie/controller/GebruikerControllerTest.java
+++ src/test/java/MakePlannieWork/Plannie/controller/GebruikerControllerTest.java
@@ -1,39 +1,47 @@
 package MakePlannieWork.Plannie.controller;
 
+import org.junit.runner.RunWith;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
 public class GebruikerControllerTest {
 
-    private WebDriver driver;
+    @BeforeEach
+    public void setUp() {
 
-//    @BeforeEach
-//    public void setUp() {
-//        System.setProperty("webdriver.chrome.driver", "Algemeen/chromedriver.exe");
-//
-//        driver = new ChromeDriver();
-//    }
-//
-//    @AfterEach
-//    public void tearDown() {
-//        driver.close();
-//    }
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
 
     @Test
-    public void testGoogleSearch() throws InterruptedException {
-        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
+    public void testRegistreren() throws InterruptedException {
+
         System.setProperty("webdriver.chrome.driver", "Algemeen/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com/");
 
-        Thread.sleep(5000);  // Let the user actually see something!
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("ChromeDriver");
-        searchBox.submit();
-        Thread.sleep(5000);  // Let the user actually see something!
+        driver.get("http://localhost:8080/registreren");
+
+        Thread.sleep(1000);
+        driver.findElement(By.name("voornaam")).sendKeys("Daniel");
+        driver.findElement(By.name("achternaam")).sendKeys("Kuperus");
+        driver.findElement(By.name("email")).sendKeys("test.testing@daniel.com");
+        driver.findElement(By.name("wachtwoord")).sendKeys("echtgeen123");
+        driver.findElement(By.name("trancientWachtwoord")).sendKeys("echtgeen123");
+        Thread.sleep(5000);
+
         driver.quit();
     }
-
 }
