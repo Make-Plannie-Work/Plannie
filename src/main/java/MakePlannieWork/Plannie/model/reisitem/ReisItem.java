964a5096887e092e476503f3193ccc3c133e51bb
tabithakainama <tabitha70@hotmail.com>
2019-12-18 14:28:44 +0100

    ReisItem class aangemaakt en gevuld.

--- src/main/java/MakePlannieWork/Plannie/model/reisitem/ReisItem.java
+++ src/main/java/MakePlannieWork/Plannie/model/reisitem/ReisItem.java
@@ -1,14 +1,65 @@
 package MakePlannieWork.Plannie.model.reisitem;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import MakePlannieWork.Plannie.model.Groep;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+/**
+ * ReisItem class aangemaakt. Is composite. Heeft 1 op veel relatie met Groep.
+ */
+
+
 
 @Entity
-public abstract class ReisItem {
+@Table(name = "reis_item")
+public class ReisItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer reisItemId;
+
+    private String naam;
+    private String datum;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "groep_id", referencedColumnName = "groep_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Groep groep;
+
+    public Integer getReisItemId() {
+        return reisItemId;
+    }
+
+    public void setReisItemId(Integer reisItemId) {
+        this.reisItemId = reisItemId;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public Groep getGroep() {
+        return groep;
+    }
+
+    public void setGroep(Groep groep) {
+        this.groep = groep;
+    }
+
+
+
 }
