4183b9149a48e182d87305be407898d681ddff4b
tabithakainama <tabitha70@hotmail.com>
2019-12-20 11:37:57 +0100

    Notitie class gevuld. En OneToMany relatie gelegd in reisitem met Notitie

--- src/main/java/MakePlannieWork/Plannie/model/reisitem/Notitie.java
+++ src/main/java/MakePlannieWork/Plannie/model/reisitem/Notitie.java
@@ -1,4 +1,32 @@
 package MakePlannieWork.Plannie.model.reisitem;
 
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+
+@Entity
+@Table(name = "notitie")
 public class Notitie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer notitieId;
+
+    private String tekst;
+
+    public Integer getNotitieId() {
+        return notitieId;
+    }
+
+    public void setNotitieId(Integer notitieId) {
+        this.notitieId = notitieId;
+    }
+
+    public String getTekst() {
+        return tekst;
+    }
+
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
+    
 }
