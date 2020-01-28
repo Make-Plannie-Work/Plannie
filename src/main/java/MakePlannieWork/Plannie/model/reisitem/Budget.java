bc27bc92b349455eb6f1196099f8b083db1228a5
tabithakainama <tabitha70@hotmail.com>
2020-01-28 09:41:54 +0100

    Budget class variabele Bedrag toegevoegd

--- src/main/java/MakePlannieWork/Plannie/model/reisitem/Budget.java
+++ src/main/java/MakePlannieWork/Plannie/model/reisitem/Budget.java
@@ -1,4 +1,14 @@
 package MakePlannieWork.Plannie.model.reisitem;
 
-public class Budget {
+public class Budget extends ReisItem {
+
+    public int getBedrag() {
+        return bedrag;
+    }
+
+    public void setBedrag(int bedrag) {
+        this.bedrag = bedrag;
+    }
+
+    private int bedrag;
 }
