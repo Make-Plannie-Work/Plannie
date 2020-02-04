d93dd8b90e887d4eabf164096634bfd92062001f
DanielKuperus <daniel.kuperus@gmail.com>
2020-02-04 14:34:08 +0100

    Bij wijze van test op travis.com wordt een waarde van application.properties nu @Value ingevoegd, bij GroepControllerTest.java.

--- src/test/java/MakePlannieWork/Plannie/controller/GroepControllerTest.java
+++ src/test/java/MakePlannieWork/Plannie/controller/GroepControllerTest.java
@@ -3,6 +3,7 @@ package MakePlannieWork.Plannie.controller;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +12,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class GroepControllerTest {
+
+    @Value("${plannie.sleutel}")
+    private String sleutel;
 
     @Autowired
     private GroepController groepController;
