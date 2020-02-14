47e73fe5c289e992fc3acb60054a6c6053ee974e
tabithakainama <tabitha70@hotmail.com>
2020-02-14 14:07:02 +0100

    @author toegevoegd en ongebruikte imports en methodes verwijderd

--- src/main/java/MakePlannieWork/Plannie/repository/GebruikerVerificatieRepository.java
+++ src/main/java/MakePlannieWork/Plannie/repository/GebruikerVerificatieRepository.java
@@ -1,14 +1,12 @@
 package MakePlannieWork.Plannie.repository;
 
+/**
+ * @Author Tabitha Krist
+ */
+
 import MakePlannieWork.Plannie.model.Gebruiker;
 import MakePlannieWork.Plannie.model.GebruikerVerificatieToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.Date;
-import java.util.stream.Stream;
 
 public interface GebruikerVerificatieRepository extends JpaRepository<GebruikerVerificatieToken, Integer> {
 
@@ -16,11 +14,4 @@ public interface GebruikerVerificatieRepository extends JpaRepository<GebruikerV
 
     GebruikerVerificatieToken findByGebruiker(Gebruiker gebruiker);
 
-    Stream<GebruikerVerificatieToken> findAllByExpiryDateLessThan(Date now);
-
-    void deleteByExpiryDateLessThan(Date now);
-
-    @Modifying
-    @Query("delete from GebruikerVerificatieToken where expiryDate <=?1")
-    void deleteAllExpiredSince(Date now);
 }
